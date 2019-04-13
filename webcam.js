0406c538065af716c3ae4388dd779b27bc1cad64
Rishabh Arya <34997913+rishabharya1422@users.noreply.github.com>
2019-04-14 04:03:46 +0530

    Add files via upload

---
+++ webcam.js
@@ -0,0 +1,26 @@
+//Written by Rishabh Arya
+//Free Distribution 
+//v2.0.19
+
+
+
+const webcamElement = document.getElementById('webcamPhotage');
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
